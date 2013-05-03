0c15fe040cb5c0c7b7fa9e26945d6761bbfc3997
Kevin Thompson <kevin@kevinthompson.info>
2013-05-03 11:42:05 -0700

    Revert to Rails 3.2.13 from Rails 4.0.0rc1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Photeasy::Application.config.secret_key_base = 'c67b2e5afb81f78f2d2bf90e4ed33e9f820af81459d3f7e33ade69744ea55e924034386ce2e4af1b137b52767aa814a63c2e359ae2407415815e4cd283cf70dd'
+Photeasy::Application.config.secret_token = '9a3b3198435faa971dd18f91bd76d2c3cc14115b3914edf686c83dbac200a0ef7edcb6ba4afc9434279c4f8ba2fc51741e6447fa16751a2d1492c98fa4deba13'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Photeasy::Application.config.session_store :cookie_store, key: '_photeasy_session'
+Photeasy::Application.config.session_store :cookie_store, key: '_Photeasy_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Photeasy::Application.config.session_store :active_record_store
