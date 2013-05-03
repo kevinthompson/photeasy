d7c2af6fcdbbb79e790b14058894b9c06134610f
Kevin Thompson <kevin@kevinthompson.info>
2013-05-03 12:04:04 -0700

    Add Basic ActiveAdmin install.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Photeasy::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :users
+  ActiveAdmin.routes(self)
   root to: 'high_voltage/pages#show', id: 'index'
 end
