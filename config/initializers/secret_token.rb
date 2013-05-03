2bc760e7c0e82c4aabd88cab80a2c60411aa6573
Kevin Thompson <kevin@kevinthompson.info>
2013-05-03 10:03:54 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photeasy::Application.config.secret_key_base = 'c67b2e5afb81f78f2d2bf90e4ed33e9f820af81459d3f7e33ade69744ea55e924034386ce2e4af1b137b52767aa814a63c2e359ae2407415815e4cd283cf70dd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Photeasy::Application.config.session_store :cookie_store, key: '_photeasy_session'
