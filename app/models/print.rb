4db35d5a52a15670c30ac16cfe68c0a3f198a219
Kevin Thompson <kevin@kevinthompson.info>
2013-05-25 22:12:26 -0700

    Add print validation.

--- app/models/print.rb
+++ app/models/print.rb
@@ -7,4 +7,7 @@ class Print < ActiveRecord::Base
 
   validates :order_id, :photo_id, :quantity, :size, presence: true
   validates :size, inclusion: { in: VALID_SIZES, message: "%{value} is not a valid size." }
+  validates :quantity, numericality: { greater_than: 0 }
+  validates :photo_id, uniqueness: { scope: [:order_id, :size] }
+  validates :size_id, uniqueness: { scope: [:order_id, :photo_id] }
 end
