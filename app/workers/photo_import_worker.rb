00ea4306ca132979d77da279bbd39a6440b6e712
Kevin Thompson <kevin@kevinthompson.info>
2013-05-26 23:09:30 -0700

    Move import logic out of Sidekiq worker.

--- app/workers/photo_import_worker.rb
+++ app/workers/photo_import_worker.rb
@@ -1,15 +1,8 @@
 class PhotoImportWorker < BaseWorker
 
-  def perform(user_id, notification_channel = nil)
-    @notification_channel = notification_channel
+  def perform(user_id)
     user = User.find(user_id)
-    user.photos.destroy_all
-    user.dropbox.photos.each do |file|
-      photo = user.photos.where(provider: :dropbox, provider_id: file.id).first_or_initialize
-      photo.update_attributes(url: file.path, filename: file.filename)
-      photo.upload_from_dropbox
-    end
-    user.update_attribute(:photos_imported_at, Time.now)
+    user.import_photos_from_providers if user.present?
   end
 
 end
