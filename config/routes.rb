3baa035573a1902cec8fb7f36e4bbec7b2489887
Kevin Thompson <kevin@kevinthompson.info>
2013-05-28 11:09:26 -0700

    Disable app subdomain routes in production.

--- config/routes.rb
+++ config/routes.rb
@@ -2,36 +2,38 @@ require 'sidekiq/web'
 
 Photeasy::Application.routes.draw do
 
-  # App
-  constraints subdomain: 'app' do
+  if !Rails.env.production?
+    # App
+    constraints subdomain: 'app' do
 
-    # Index
-    root to: 'pages#show', id: 'app'
+      # Index
+      root to: 'pages#show', id: 'app'
 
-    # Administration
-    ActiveAdmin.routes(self)
+      # Administration
+      ActiveAdmin.routes(self)
 
-    # Background Jobs
-    admin_user = lambda { |request| request.env['warden'].authenticate? and request.env['warden'].user.is_admin? }
-    constraints admin_user do
-      mount Sidekiq::Web => '/sidekiq'
-    end
+      # Background Jobs
+      admin_user = lambda { |request| request.env['warden'].authenticate? and request.env['warden'].user.is_admin? }
+      constraints admin_user do
+        mount Sidekiq::Web => '/sidekiq'
+      end
 
-    # Authentication
-    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-    devise_scope :user do
-      get '/sign_in' => 'devise/sessions#new'
-      get '/sign_out' => 'devise/sessions#destroy'
-    end
+      # Authentication
+      devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+      devise_scope :user do
+        get '/sign_in' => 'devise/sessions#new'
+        get '/sign_out' => 'devise/sessions#destroy'
+      end
 
-    # API
-    namespace :api, format: true, constraints: { format: :json } do
-      namespace :v1 do
-        resources :photos, only: [:index]
-        resources :users, only: [:show] do
-          resources :collections
+      # API
+      namespace :api, format: true, constraints: { format: :json } do
+        namespace :v1 do
+          resources :photos, only: [:index]
+          resources :users, only: [:show] do
+            resources :collections
+          end
+          resources :collections, only: [:show]
         end
-        resources :collections, only: [:show]
       end
     end
   end
