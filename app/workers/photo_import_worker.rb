4c9d2de1a35d3f6246b38e604c1f2b7ce8dc5606
Kevin Thompson <kevin@kevinthompson.info>
2013-07-04 10:35:24 -0700

    Resolve indifferent hash access issue with sidekiq workers.

--- app/workers/photo_import_worker.rb
+++ app/workers/photo_import_worker.rb
@@ -1,6 +1,7 @@
 class PhotoImportWorker < BaseWorker
 
   def perform(args={})
+    args = args.with_indifferent_access
     user = User.find(args[:user_id])
     user.import_photos if user.present? && !user.importing_photos
   end
