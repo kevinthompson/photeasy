b851110ae90b0f1213b98417b141550f67f5dcd5
Kevin Thompson <kevin@kevinthompson.info>
2013-07-04 21:49:28 -0700

    Update permitted parameters for Order

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -9,7 +9,7 @@ class API::V1::OrdersController < API::V1::BaseController
   private
 
   def order_params
-    params.require(:order).permit(:album_id, :status, :user_id, prints_attributes: [:order_id, :photo_id, :quantity, :size])
+    params.require(:order).permit(:album_id, :share_id, :user_id, prints_attributes: [:photo_id, :quantity, :size])
   end
 
 end
